abd7e8137dfbdf68d23a3f93b4d8fa59465ae909
runeunhjem <runeunhjem@gmail.com>
2023-12-09 17:31:00 +0100

    Updates favicon links

--- src/js/variables/constants.mjs
+++ src/js/variables/constants.mjs
@@ -3,8 +3,8 @@ export const currentProfileName = localStorage.getItem("currentProfileName");
 export const accessToken = localStorage.getItem("accessToken");
 export const isLoggedIn = localStorage.getItem("isLoggedIn");
 export const favoritesLink = document.querySelector(".icon-favorites");
-export const globalLimit = 0;
-export const globalMaxTotalListings = 100;
+export const globalLimit = 100;
+export const globalMaxTotalListings = 1000;
 
 export const pastelColors = [
   "#fbb4ae", // pastel pink
