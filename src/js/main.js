03b44ff026b65449d132b151d90fcb4d69a9870e
runeunhjem <runeunhjem@gmail.com>
2023-12-13 12:49:33 +0100

    Removes autoscroll from listing cards as it removed the ends in countdown timer

--- src/js/main.js
+++ src/js/main.js
@@ -80,11 +80,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     const elements = document.querySelectorAll(".categories-container");
     elements.forEach(el => loopScroll(el, 1.5));
 
-    const elements2 = document.querySelectorAll(
-      ".latest-auctions, .ends-soon-auctions, .profile-auctions"
-    );
-    elements2.forEach(el => loopScroll(el, 0.5));
-
     setTimeout(() => {
       // Wait for images to load
       initializeAllCarousels();
